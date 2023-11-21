511b9dcd5ddb780e2b7f3370cc3509ea98ff8cf3
Piotr Kulpa <piotr.kulpa@intive.com>
2023-11-21 21:22:34 +0100

    Update update-commands.js

--- queries/update-commands.js
+++ queries/update-commands.js
@@ -1,3 +1,5 @@
-db.flightData.updateOne({distance: 1200}, {$set: {marker: "delete"}})
+db.flightData.updateOne({distance: 1200}, {$set: {marker: "delete"}}) // this will only override marker
 
-db.flightData.updateMany({}, {$set: {marker: "toDelete"}})
+db.flightData.updateMany({}, {$set: {marker: "toDelete"}}) // this will only override marker
+
+db.flightData.update({_id: ObjectId("123123")}, {delayed: false}) // this will override all data except of id
