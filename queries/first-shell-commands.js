cdbc1b2165c3fb95c7cbcbffe50ebe04e2831fb6
Piotr Kulpa <piotr.kulpa@intive.com>
2023-11-21 10:29:09 +0100

    Update first-shell-commands.js

--- queries/first-shell-commands.js
+++ queries/first-shell-commands.js
@@ -2,4 +2,10 @@ db.products.insertOne({name: "A book", price: 12.99})
 
 db.products.insertOne({name: "T-shirt", price: 20.99, description: "This is high quality T-shirt"})
 
-shop> db.products.insertOne({name: "Computer", price: 1220.99, description: "This is high quality PC", details: {cpu: "Intel i7", memory: 32}})
+db.products.insertOne({name: "Computer", price: 1220.99, description: "This is high quality PC", details: {cpu: "Intel i7", memory: 32}})
+
+db.flightData.insertOne({departureAirport: "TXL", arrivalAirport: "LHR"})
+
+db.flightData.insertOne({departureAirport: "TXL", arrivalAirport: "LHR", _id: "txl-lhr-1"})
+
+db.flightData.insertOne({departureAirport: "MUC", arrivalAirport: "SFO", aircraft: "Airbus A380", distance: 1200, intercontinental: true})
