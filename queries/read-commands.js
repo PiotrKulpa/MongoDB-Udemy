c6e9967c83a87c0d84a10a5ba5429fdc6537c391
Piotr Kulpa <piotr.kulpa@intive.com>
2023-11-22 11:14:08 +0100

    Update read-commands.js

--- queries/read-commands.js
+++ queries/read-commands.js
@@ -1,1 +1,4 @@
+// find does not return all documents, it returns some documents and coursor objecs with some methods like "it"
 db.flightData.find({distance: {$gt: 1000}}) // $gt stands for greater than
+
+db.flightData.find({"status.details": "on-time"}) // accessing structured data
